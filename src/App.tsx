12d997d6d75a77fa211821ac98e7dfb5853c291a
omar-ea <126994619+WalidOEa@users.noreply.github.com>
2024-12-10 23:33:50 +0000

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import './global.css';
+
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+)
 }
 
 export default App
