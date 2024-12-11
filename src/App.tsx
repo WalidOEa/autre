0b181fdf19cbee8283266a92d9c9a98da95f5d89
omar-ea <126994619+WalidOEa@users.noreply.github.com>
2024-12-11 21:36:59 +0000

    Refactor dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { Routes, Route } from 'react-router-dom';
 
 import './assets/global.css';
+import AuthLayout from './_auth/AuthLayout';
+import SigninForm from './_auth/_forms/SigninForm';
+import SignupForm from './_auth/_forms/SignupForm';
+import Home from './_root/pages/Home';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
     <main className="flex h-screen">
         <Routes>
             {/* public routes */}
-            <Route path="/sign-in" element={<SigninForm />} />
+            <Route element={<AuthLayout />}>
+                <Route path="/sign-in" element={<SigninForm />} />
+                <Route path="/sign-up" element={<SignupForm />} />
+            </Route>
 
             {/* private routes */}
-            <Route index element={<Home />}/>
+            <Route element={<RootLayout />}>
+                <Route index element={<Home />}/>
+            </Route>
         </Routes>
     </main>
 )
